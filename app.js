dc3f23e120b19f4525d109b5375a5611663e8d9c
mothana404 <https://github.com/mothana404>
2023-10-10 03:18:29 +0300

    new22

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// const { error } = require("server/router");
 class Product {
     constructor (title, price, image){
         this.title = title;
@@ -8,16 +9,14 @@ class Product {
         let cardContainer = document.querySelector(".cards-container");
         let card = document.createElement("div");
         card.innerHTML = 
-        `
-        <div class="card" style="width: 18rem;">
+        `<div class="card" style="width: 18rem;">
             <img src="${this.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5>${this.title}</h5>
                 <p class="card-text">${this.price}</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-        </div>
-        `;
+        </div>`;
         cardContainer.appendChild(card);
     }
 };
@@ -34,3 +33,82 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => console.error("Error fetching products:", error));
 });
+
+//this is for post
+let submit = document.querySelector(".submit");
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    let title = document.querySelector("input[name = title]").value;
+    let content = document.querySelector("input[name = content]").value;
+    fetch("http://localhost:3000/posts",{
+        method : "POST",
+        headers : {
+            "Content-type": "application/json",
+        },
+        body : JSON.stringify({
+            "id" : "",
+            "title" : `${title}`,
+            "content" : `${content}`,
+        }),
+    })
+    .catch(error => console.log("there is an error with post"));
+});
+
+//this is to display the cards
+let container = document.querySelector(".new-cards");
+document.addEventListener("DOMContentLoaded", () =>{
+    fetch("http://localhost:3000/posts")
+    .then(response => response.json())
+    .then(data => {
+        data.map((element) =>{
+            let card = document.createElement("div");
+            card.innerHTML = 
+            `<div>
+                <img src = "">
+                <span class = anything>
+                    <span contenteditable="true" id = "title-edit">${element.title}</span>
+                    <span contenteditable="true" id = "content-edit">${element.content}</span>
+                    <button id = ${element.id} class = "remove" onclick = "remove(this)">remove</button>
+                    <button id = ${element.id} class = "update" onclick = "update(this)">update</button>
+                </span>
+            </div>`;
+            // container.insertAdjacentHTML("beforeend", card);
+            container.appendChild(card);
+        })
+    })
+    .catch(error => console.log("there is an error with git"));
+});
+
+//this is the remove
+function remove(e, ele){
+    e.preventDefault();
+    let idid = ele.id;
+    console.log(idid);
+    fetch(`http://localhost:3000/posts/${cardid}`, {
+        method : "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            // Additional headers if needed
+        },
+    })
+    .catch(error => console.log("there is an error with Delete"));
+};
+
+//this is to update
+function update(e){
+    let cardid = e.id;
+    console.log(cardid);
+    let newtitle = document.querySelector("#title-edit").innerHTML;
+    let newcontent = document.querySelector("#content-edit").innerHTML;;
+    fetch(`http://localhost:3000/posts/${cardid}`, {
+        method : 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body : JSON.stringify({
+            "title" : `${newtitle}`,
+            "content" : `${newcontent}`,
+        }),
+    })
+    .catch(error => console.log("there is an error with patch"));
+};
